84a1e1c94eb998413b3f00c8e3f9fd547e9147c9
changer <dingjinchengyx@163.com>
2016-07-10 23:43:21 +0800

    1

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var express = require('express');  
   
-var app = express.createServer();  
+var app = express();  
   
-// Express 程序配置  
-app.configure(function(){  
-  app.set('views', __dirname + '/views');  
-  app.set('view engine', 'ejs');  
-  app.use(app.router);  
-  app.use(express.static(__dirname + '/content'));  
-});  
-  
+app.set('views', __dirname + '/views');  
+app.engine('html', require('ejs').renderFile);
+app.use(app.router);  
+app.use(express.static(__dirname + '/content')); 
+
 // url路由  
 app.get('/', function(req, res){  
   res.render('index', {  
