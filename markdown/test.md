de9a31acd79aec9054fa469403baefc3ae809c38
changer <dingjinchengyx@163.com>
2016-10-18 07:39:22 +0800

    add markdown

---
+++ markdown/test.md
@@ -0,0 +1,25 @@
+# JSqlParser
+
+```xml
+<repositories>
+     <repository>
+         <id>jsqlparser-snapshots</id>
+         <snapshots>
+             <enabled>true</enabled>
+         </snapshots>
+         <url>https://oss.sonatype.org/content/groups/public/</url>
+     </repository>
+</repositories>
+```
+This repositories releases will be synched to maven central. Snapshots remain at sonatype.
+
+And this is the dependency declaration in your pom:
+```xml
+<dependency>
+	<groupId>com.github.jsqlparser</groupId>
+	<artifactId>jsqlparser</artifactId>
+	<version>0.9.6</version>
+</dependency>
+```
+
+
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');  
-  
+var markdown = require("markdown").markdown;
+var fs = require("fs");
+
 var app = express();  
-  
 app.set('views', __dirname + '/views');  
 app.set('view engine', 'ejs');  
 //app.engine('ejs', require('ejs').renderFile);
@@ -13,10 +14,23 @@ app.get('/', function(req, res){
     title: 'Blog'  
   });  
 });  
+
 app.get('/document',function(req,res) {
-	res.render('doc', {
-		title: 'document'
-	})
+	res.render('doc',{'markdown':'/markdown/test.js'});
+});
+app.param('markdown',function(req,res,next,value) {
+	var fname = value.slice(0,value.lastIndexOf('.')) + '.md';
+	console.log("request " + fname);
+
+	res.write("$('#markdown').append(\"");
+	var md = fs.readFileSync(__dirname + '/markdown/' + fname);
+	data = markdown.toHTML(md.toString('utf-8'));
+	res.write(data);
+	res.write("\");");
+	next();	
+});
+app.use('/markdown/:markdown',function(req,res,next) {
+	res.end();
 });
   
 app.listen(8090); 
